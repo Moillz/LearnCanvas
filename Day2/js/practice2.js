2771cc94a718199ec0075090c6d462df99320bd2
Moillz <dai1951490147@outlook.con>
2019-09-26 17:29:32 +0800

    commit

---
+++ Day2/js/practice2.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+  var canvas = document.getElementById('canvas');
+  canvas.width = 800;
+  canvas.height = 600;
+  var context = canvas.getContext('2d');
+
+  context.beginPath();
+  context.moveTo(100, 100);
+  
+
+}
